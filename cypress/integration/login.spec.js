e7eb230b409d6f29d96e760751abdba23763c4f8
Marcos <marcos.policeno@br.experian.com>
2022-02-18 14:32:59 -0300

    testes pre cadastro e produtos

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,22 +1,26 @@
 /// <reference types="cypress"/>
 
 context('Funcionlidade Login', () => {
-    it('Deve fazer login com sucesso', () => {
+
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    afterEach(() => {
+        cy.screenshot()
+    });
+    it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.page-title').should('have.text', 'Minha conta')
     });
     it('Deve exibir uma mensagem de erro ao inserir o usuário inválidos', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@teste1.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-error > li').should('contain', 'Endereço de e-mail desconhecido')
     });
     it('Deve exibir uma mensagem de erro ao inserir a senha inválida', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com1')
         cy.get('.woocommerce-form > .button').click()
---
+++ cypress/integration/pre-cadastro.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress"/>
+import { faker } from '@faker-js/faker';
+
+describe('Funcionalidade Pré Cadastro', () => {
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    it('Devo criar pré cadastro', () => {
+
+        let nomeFaker = faker.name.firstName()
+        let lastNameFaker = faker.name.lastName()
+        let email = faker.internet.email()
+
+        cy.get('#reg_email').type(email)
+        cy.get('#reg_password').type('teste@teste.com')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(nomeFaker)
+        cy.get('#account_last_name').type(lastNameFaker)
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
+    });
+});
